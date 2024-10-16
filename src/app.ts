611d6f5f9c36f66bf7ecbb4743f1df08d108aa86
JomnoiZ <kamanun.m@gmail.com>
2024-10-16 15:49:12 +0700

    remove port env

--- src/app.ts
+++ src/app.ts
@@ -53,6 +53,6 @@ app.use('/ap', apRoutes);
 app.use('/user', userRoutes);
 app.use(errorRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(3001, () => {
+    console.log(`Server running on port 3001`);
 });
